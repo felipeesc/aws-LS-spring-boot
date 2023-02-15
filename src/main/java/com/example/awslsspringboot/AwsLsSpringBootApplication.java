e911d5da06c3e64cf8945924e7007e02f764b1dc
Felipe Carvalho <FelipeCarvalho@192.168.1.8>
2023-02-15 17:23:52 -0300

    aplicação com Spring Boot que funciona com os recursos do LocalStack e da AWS

---
+++ src/main/java/com/example/awslsspringboot/AwsLsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.awslsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsLsSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/awslsspringboot/AwsLsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awslsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsLsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
