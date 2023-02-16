9c17d495784f073d2680f52d2620ef1b00758cb5
Felipe Carvalho <felipee_sc@hotmail.com>
2023-02-16 09:17:05 -0300

    implementando localstack docker

--- src/main/java/com/example/awslsspringboot/AwsLsSpringBootApplication.java
+++ src/main/java/com/example/awslsspringboot/AwsLsSpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AwsLsSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AwsLsSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AwsLsSpringBootApplication.class, args);
+    }
 
 }
